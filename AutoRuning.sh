a95e8f0d878338f2652aef54516d01b78b0bfb5f
AmirKaLan <32092233+CerNerCompany@users.noreply.github.com>
2018-12-03 16:02:22 +0000

    Update AutoRuning.sh

--- AutoRuning.sh
+++ AutoRuning.sh
@@ -2,6 +2,6 @@
 echo "\e[101m                              Launching....                          \e[00;37;40m"
 while true; do
 echo "\e[1m\e[100mnice: process ID value: ./CPM                          "
-nice -n 20 ./CPM l
+nice -n -20 ./CPM l
 sleep 5
 done
