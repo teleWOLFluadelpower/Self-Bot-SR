24c8b3a6a94bca6b061390575ded46da94db421f
Amir Bagheri <32092233+KhodeAmir@users.noreply.github.com>
2019-06-19 08:49:54 +0430

    Delete AutoRuning.sh

--- AutoRuning.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-echo "\e[101m                              Launching....                          \e[00;37;40m"
-while true; do
-echo "\e[1m\e[100mnice: process ID value: ./CPM                          "
-nice -n -20 ./CPM l
-sleep 5
-done
